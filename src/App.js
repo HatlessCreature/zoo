7754250ad1f5c6b936d84056c179ea27b9e1379f
Vladimir Simin <vladimir.simin@gmail.com>
2021-11-09 19:14:53 +0100

    task 2 - unknown date of birth functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -7,24 +7,31 @@ function App() {
     { id: "2", species: "Canis lupus", name: "Reks", dateOfBirth: new Date() },
     { id: "3", species: "Panthera tigris", name: "Meca", dateOfBirth: new Date() },
     { id: "4", species: "Panthera leo", name: "Kori", dateOfBirth: new Date() },
-    { id: "5", species: "Lynx lynx", name: "Rogonja", dateOfBirth: new Date() }
+    { id: "5", species: "Lynx lynx", name: "Rogonja", dateOfBirth: new Date() },
+    { id: "6", species: "Lynx lynx", name: "Rogonja 2", dateOfBirth: null },
+    { id: "7", species: "Lynx lynx", name: "Rogonja 3", }
   ];
+
 
   return (
     <div>
       <table>
-        <tr>
-          <th>Species</th>
-          <th>Name</th>
-          <th>Date of birth</th>
-        </tr>
-        {animals.map((animal) => (
-          <tr key={animal.id}>
-            <td>{animal.species}</td>
-            <td>{animal.name}</td>
-            <td>{animal.dateOfBirth.toISOString()}</td>
+        <thead>
+          <tr>
+            <th>Species</th>
+            <th>Name</th>
+            <th>Date of birth</th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {animals.map((animal) => (
+            <tr key={animal.id}>
+              <td>{animal.species}</td>
+              <td>{animal.name}</td>
+              <td>{animal.dateOfBirth ? (animal.dateOfBirth.toISOString()) : ("Nepoznat")}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
