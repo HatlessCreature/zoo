5dfd19743d013fbce431eacb69eddb408309d7ec
Vladimir Simin <vladimir.simin@gmail.com>
2021-11-09 18:53:25 +0100

    task1 - animals table added

--- src/App.js
+++ src/App.js
@@ -2,22 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const animals = [
+    { id: "1", species: "Usrus Arctos", name: "Djole", dateOfBirth: new Date() },
+    { id: "2", species: "Canis lupus", name: "Reks", dateOfBirth: new Date() },
+    { id: "3", species: "Panthera tigris", name: "Meca", dateOfBirth: new Date() },
+    { id: "4", species: "Panthera leo", name: "Kori", dateOfBirth: new Date() },
+    { id: "5", species: "Lynx lynx", name: "Rogonja", dateOfBirth: new Date() }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table>
+        <tr>
+          <th>Species</th>
+          <th>Name</th>
+          <th>Date of birth</th>
+        </tr>
+        {animals.map((animal) => (
+          <tr key={animal.id}>
+            <td>{animal.species}</td>
+            <td>{animal.name}</td>
+            <td>{animal.dateOfBirth.toISOString()}</td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
