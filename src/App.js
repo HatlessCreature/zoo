90ac8f2d83146e8632a77247fcd11fbf972aca52
Vladimir Simin <vladimir.simin@gmail.com>
2021-11-09 20:00:16 +0100

    task 3 - delete button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 // import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  const animals = [
+  const animalsList = [
     { id: "1", species: "Usrus Arctos", name: "Djole", dateOfBirth: new Date() },
     { id: "2", species: "Canis lupus", name: "Reks", dateOfBirth: new Date() },
     { id: "3", species: "Panthera tigris", name: "Meca", dateOfBirth: new Date() },
@@ -12,6 +13,16 @@ function App() {
     { id: "7", species: "Lynx lynx", name: "Rogonja 3", }
   ];
 
+  const [animals, setAnimals] = useState(animalsList);
+
+  const deleteRow = (id) => {
+    var updatedAnimals = [...animals];
+    var indexToRemove = updatedAnimals.findIndex(x => x.id == id);
+    if (indexToRemove > -1) {
+      updatedAnimals.splice(indexToRemove, 1)
+      setAnimals(updatedAnimals);
+    }
+  };
 
   return (
     <div>
@@ -29,6 +40,9 @@ function App() {
               <td>{animal.species}</td>
               <td>{animal.name}</td>
               <td>{animal.dateOfBirth ? (animal.dateOfBirth.toISOString()) : ("Nepoznat")}</td>
+              <td>
+                <button onClick={() => deleteRow(animal.id)}>Delete</button>
+              </td>
             </tr>
           ))}
         </tbody>
